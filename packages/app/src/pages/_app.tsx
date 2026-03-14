df0dbed6b28564342b7c0bca6b05ea8b111b3616
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-14 20:05:14 +0700

    update prettier

--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -5,29 +5,29 @@ import Head from 'next/head';
 import { FC } from 'react';
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
 });
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-  return (
-    <>
-      <Head>
-        <title>Palindrome</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-      </Head>
-      <div
-        className={`${geistSans.className} ${geistMono.className} bg-neutral-900 text-neutral-100`}>
-        <Component {...pageProps} />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Palindrome</title>
+				<link rel="icon" href="/favicon.ico" />
+				<link rel="manifest" href="/manifest.json" />
+			</Head>
+			<div
+				className={`${geistSans.className} ${geistMono.className} bg-neutral-900 text-neutral-100`}>
+				<Component {...pageProps} />
+			</div>
+		</>
+	);
 };
 
 export default App;
