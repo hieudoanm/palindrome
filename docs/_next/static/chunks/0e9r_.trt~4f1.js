fd83672087fb824fda83e8197267461d57eea2b9
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-28 10:46:50 +0700

    Add Tauri

--- docs/_next/static/chunks/0e9r_.trt~4f1.js
+++ docs/_next/static/chunks/0e9r_.trt~4f1.js
@@ -2,7 +2,7 @@ __turbopack_load_page_chunks__("/_app", [
   "static/chunks/0el87rcslki37.js",
   "static/chunks/0nd086j~znsgy.js",
   "static/chunks/0dx305gvqgt5_.js",
-  "static/chunks/08iua4ebjd2iq.css",
+  "static/chunks/0l_p0ny39f~xm.css",
   "static/chunks/0p44cybxe7kuv.css",
-  "static/chunks/turbopack-06203~pvwjguc.js"
+  "static/chunks/turbopack-0vca3hbktnx89.js"
 ])
